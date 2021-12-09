86ce8315b433eefa8a202dc52ecdf30114b7d61c
Dorian Brown <brown.3794@gmail.com>
2021-12-09 17:54:24 -0500

    feat: :sparkles: Add markup for layout header

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,33 @@
+<script>
+</script>
+
+<svelte:head>
+	<!-- head content -->
+</svelte:head>
+
+<header>
+	<nav id="site-navigation">
+		<div class="jump-to-section_container" />
+		<div class="primary-nav_container">
+			<ul class="primary-nav_links">
+				//TODO: Fix a tags
+				<li><a href="/about" />About</li>
+				<li><a href="/articles" />Articles</li>
+				<li><a href="/work" />Work</li>
+				<li><a href="/contact" />Contact</li>
+			</ul>
+			//TODO: replace button with hamburger component
+			<button />
+		</div>
+	</nav>
+</header>
+<main>
+	<slot />
+</main>
+<footer />
+
+<style>
+	#site-navigation {
+		display: flex;
+	}
+</style>
